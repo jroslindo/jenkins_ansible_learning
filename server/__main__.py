65f3093214dc3dd9f6f0389e0799ce384db28dba
jroslindo <jroslindotab@gmail.com>
2024-05-22 19:47:56 -0300

    testing

--- server/__main__.py
+++ server/__main__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1><h1>Final test working!! lets try supervisor</h1>'
+    return '<h1>Hello, World!</h1><h1>Final test working!!</h1>'
 
 
 def stop_flask(pid):
